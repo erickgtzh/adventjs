f92df206ee09303dd249ca5a211ea4681fdb87c2
Erick Gutiérrez <0193662@up.edu.mx>
2021-12-08 17:35:04 -0600

    Add files via upload

---
+++ #7-buscando.js
@@ -0,0 +1,44 @@
+function contains(store, product) {
+  for(let key in store){
+    if(typeof store[key] === 'object'){
+      if(contains(store[key], product)){
+        return true;
+      }
+    }
+    else if(store[key] === product){
+      return true;
+    }
+  }
+  return false;
+}
+
+const almacen = {
+  'estanteria1': {
+    'cajon1': {
+      'producto1': 'coca-cola',
+      'producto2': 'fanta',
+      'producto3': 'sprite'
+    }
+  },
+  'estanteria2': {
+    'cajon1': 'vacio',
+    'cajon2': {
+      'producto1': 'pantalones',
+      'producto2': 'camiseta' // <- ¡Está aquí!
+    }
+  }
+}
+            
+contains(almacen, 'camiseta') // true
+
+const otroAlmacen = {
+  'baul': {
+    'fondo': {
+      'objeto': 'cd-rom',
+      'otro-objeto': 'disquette',
+      'otra-cosa': 'mando'
+    }
+  }
+}
+  
+contains(otroAlmacen, 'gameboy') // false
